27e53d81b32bb78832db8998f1b008d066c41b25
damikk19 <damikk19@gmail.com>
2025-03-18 22:36:52 +0100

    add number of words

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+    word_count = number_of_words(text)
+    print(f"{word_count} words found in the document")
 
 def get_book_text(book_path):
     with open(book_path) as f:
-        return f.read()
+        book_content = f.read()
+        return book_content
+
+def number_of_words(text):
+    words = text.split()
+    return len(words)
 
 if __name__ == "__main__":
     main()
